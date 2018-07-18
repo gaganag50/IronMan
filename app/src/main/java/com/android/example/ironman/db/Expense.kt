79d9177d592dbc5bbd33f8f8018d908bf103813f
gagan <gaganag50@gmail.com>
2018-07-18 15:20:02 +0530

    expense database created

--- app/src/main/java/com/android/example/ironman/db/Expense.kt
+++ app/src/main/java/com/android/example/ironman/db/Expense.kt
@@ -1,8 +1,18 @@
 package com.android.example.ironman.db
 
+import com.orm.SugarRecord
+
 // this is the form in which we are filling the table
-data class Expense(
-        val id: Int?,
-        val money: Int,
-        val catergory: String
-)
+class Expense() : SugarRecord<Expense>() {
+    var id: Int? = 1
+    var money: Int = 0
+    var catergory: String? = null
+
+
+
+    constructor(money: Int, category: String) : this() {
+        this.money = money
+        this.catergory = catergory
+    }
+
+}
